0945cb774207b04e55e6c0539c62e95487f438d1
wishmouse <heidi.goosen@gmail.com>
2016-05-05 13:02:09 +1200

    adding packages and working on promises

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var bodyParser = require('body-parser');
 var hbs = require('express-hbs')
 var app = express();
 var knex = require('knex');
+var Promise = this.Promise || require('promise');
+var agent = require('superagent-promise')(require('superagent'), Promise);
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+var readFileName = __dirname + '/data/readData.json'
+var writeFileName = __dirname + '/data/writeData.json'
+
+var readFilePromise = Promise.denodeify(fs.readFile)
+var writeFilePromise = Promise.denodeify(fs.writeFile)
 
 require('dotenv').config();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,9 +23,57 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', function(req, res){
-  res.render('index')
-})
+
+app.get('/scrape', function(req, res){
+
+function promiseRead () {
+  readFilePromise(readFilePromise, 'utf8')
+    .then( function (results) {
+      return results
+    })
+    .catch( function (err) {
+      throw err
+    })
+}
+
+function promiseWrite (obj) {
+  var stringified = JSON.stringify(obj)
+
+  writeFilePromise( writeFileName, stringified )
+    .catch(function(err){
+      console.log('error')
+    })
+}
+
+
+readFilePromise(readFileName, 'utf8')
+  .then ( function (readFile) {
+    var url = JSON.parse(readFile).url
+
+    return url
+  })
+  .then ( function (url) {
+
+    agent.get(url)
+  })
+  .then( function (res) {
+    var $ = cheerio.load(res.text);
+    var links = $('a')
+
+    var arr = []
+
+    for( i = 0; i < links.length; i++) {
+      arr.push(links[i].attribs.href)
+    }
+    return arr
+  })
+  .then( function (linkArr) {
+    promiseWrite( linkArr )
+  })
+  .catch( function (err) { throw err } )
+
+  res.send('check the console')
+  })
 
 
 app.listen(3000, function(){
